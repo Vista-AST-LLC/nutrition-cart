1193356ba088b596ebd9b08364ff92e8e143afa3
Louis Mikula <lemikula@gmail.com>
2026-02-24 17:55:39 -0500

    Added comment functionality to the weekly summary

--- public/js/Page3WeeklySummary.mjs
+++ public/js/Page3WeeklySummary.mjs
@@ -8,20 +8,25 @@ if (reload) {
 
 async function getDays() {
     let monday = new DayGrade(await Weekday.fromJSON(JSON.parse(localStorage.getItem('Monday'))));
-    monday.id = 'monPointsCircle';
-    monday.id2 = 'monUnderPointCircle';
+    monday.circ = 'monPointsCircle';
+    monday.underCirc = 'monUnderPointCircle';
+    monday.comments = 'monComments';
     let tuesday = new DayGrade(await Weekday.fromJSON(JSON.parse(localStorage.getItem('Tuesday'))));
-    tuesday.id = 'tuesPointsCircle';
-    tuesday.id2 = 'tuesUnderPointCircle';
+    tuesday.circ = 'tuesPointsCircle';
+    tuesday.underCirc = 'tuesUnderPointCircle';
+    tuesday.comments = 'tuesComments';
     let wednesday = new DayGrade(await Weekday.fromJSON(JSON.parse(localStorage.getItem('Wednesday'))));
-    wednesday.id = 'wedPointsCircle';
-    wednesday.id2 = 'wedUnderPointCircle';
+    wednesday.circ = 'wedPointsCircle';
+    wednesday.underCirc = 'wedUnderPointCircle';
+    wednesday.comments = 'wedComments'
     let thursday = new DayGrade(await Weekday.fromJSON(JSON.parse(localStorage.getItem('Thursday'))));
-    thursday.id = 'thursPointsCircle';
-    thursday.id2 = 'thursUnderPointCircle';
+    thursday.circ = 'thursPointsCircle';
+    thursday.underCirc = 'thursUnderPointCircle';
+    thursday.comments = 'thursComments';
     let friday = new DayGrade(await Weekday.fromJSON(JSON.parse(localStorage.getItem('Friday'))));
-    friday.id = 'friPointsCircle';
-    friday.id2 = 'friUnderPointCircle';
+    friday.circ = 'friPointsCircle';
+    friday.underCirc = 'friUnderPointCircle';
+    friday.comments = 'friComments';
 
     return [monday, tuesday, wednesday, thursday, friday];
 }
@@ -30,12 +35,23 @@ async function fillGrades() {
     let days = await getDays();
     days.forEach(day => {
         let grade = day.score[DayGrade.SCORE][Constants.SCOREAVG];
-        if (grade > 90) document.getElementById(day.id).innerHTML = 'A';
-        else if (grade > 80) document.getElementById(day.id).innerHTML = 'B';
-        else if (grade > 70) document.getElementById(day.id).innerHTML = 'C';
-        else if (grade > 60) document.getElementById(day.id).innerHTML = 'D';
-        else document.getElementById(day.id).innerHTML = 'F';
+        if (grade > 90) {
+            document.getElementById(day.circ).innerHTML = 'A';
+            document.getElementById(day.comments).innerHTML = 'Great job with this day! No more work needed here!';
+        } else if (grade > 80) {
+            document.getElementById(day.circ).innerHTML = 'B';
+            document.getElementById(day.comments).innerHTML = 'Pretty good work!';
+        } else if (grade > 70) {
+            document.getElementById(day.circ).innerHTML = 'C';
+            document.getElementById(day.comments).innerHTML = 'Not too bad, but could you do better?';
+        } else if (grade > 60) {
+            document.getElementById(day.circ).innerHTML = 'D';
+            document.getElementById(day.comments).innerHTML = 'This day needs some more work, go back to the Activity page click on this day, then click on Grade Day. It will tell you what needs fixing.';
+        } else {
+            document.getElementById(day.circ).innerHTML = 'F';
+            document.getElementById(day.comments).innerHTML = 'This day needs some more work, go back to the Activity page click on this day, then click on Grade Day. It will tell you what needs fixing.';
+        }
 
-        document.getElementById(day.id2).innerHTML = Math.round(grade);
+        document.getElementById(day.underCirc).innerHTML = Math.round(grade);
     });
 }
