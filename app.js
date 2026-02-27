a8711b14f693c0ef9c72a98b909e5458d2405b04
Louis Mikula <lemikula@gmail.com>
2026-02-27 17:30:54 -0500

    Changed profanity-cleaner path

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const PORT = 8888;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/profanity-cleaner', express.static(path.join(__dirname, 'node_modules/profanity-cleaner/dist')));
+
 app.listen(PORT, () => {
 	console.log('Server is running');
 });
